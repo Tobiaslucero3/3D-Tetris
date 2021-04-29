e423a3f592e141120866e558baa3859d9902f969
pojac3 <pojac3@gmail.com>
2021-04-29 18:02:09 -0500

    Reimplemented the difficulty system

---
+++ Assets/Scripts/Difficulty.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Difficulty : MonoBehaviour
+{
+    public static int difficulty = 1;
+    public static string difficultyTextString = "Difficulty: ";
+
+    public Text difficultyText;
+
+    public void incDifficulty ()
+    {
+        difficulty++;
+    }
+
+    public void decDifficulty ()
+    {
+        difficulty--;
+    }
+
+    void Start()
+    {
+        difficultyText.text = difficultyTextString;
+    }
+    void Update()
+    {
+        difficultyTextString = "Difficulty: " + difficulty.ToString();
+        difficultyText.text = difficultyTextString;
+    }
+}
